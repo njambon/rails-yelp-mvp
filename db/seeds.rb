2373427bf9e9af2b1175a3036424b330c201fdff
nicolas jambon <nicolas_jambon@hotmail.fr>
2021-12-14 21:28:51 +0100

    challenge 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,15 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+mavromatis = {name:'Mavrommatis', address:'rue de grece', phone_number: '0783465283', category:'french'}
+pasta = {name:'Pasta', address:'rue italie', phone_number: '0783465283', category:'italian'}
+sushi = {name:'Sushi', address:'rue du Japon', phone_number: '0783465283', category:'japanese'}
+surpriz = {name:'Surpriz', address:'rue oberkampf', phone_number: '0783465283', category:'french'}
+chinese = {name:'ChineseMan', address:'rue de Chine', phone_number: '0783465283', category:'chinese'}
+
+[mavromatis, pasta, sushi, chinese, surpriz].each do |attributes|
+  restaurant = Restaurant.create!(attributes)
+end
